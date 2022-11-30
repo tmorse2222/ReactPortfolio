9f25ce4266ac4af85280555c535a36aa3b3edb04
Tyler M <tmorse2222@gmail.com>
2022-11-29 18:58:42 -0700

    Bug fix to allow page to render properly

--- portfolio/src/components/pages/Portfolio.js
+++ portfolio/src/components/pages/Portfolio.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import a from '../images/a.png';
 
 export default function Portfolio() {
     return (
         <section>
-            <h1 id="portfolio">Portfolio</h1>
-            <p>
-                This section will contain my portfolio with 6 projects containing links to deployed app/video and github repo.
-            </p>
+            <h2 id="portfolio">Portfolio</h2>
+            <div>
+                <h3>Sequelize E-Commerce Back-End</h3>
+                <image src={a} alt="Project 11"></image>
+                <p>
+                    Repository: <a href="https://github.com/tmorse2222/E-CommerceBackEnd"> GitHub</a>
+                </p>
+                <p>
+                    Demo Video: <a href="https://drive.google.com/file/d/1Vs-x6u_6WnTYwaXEJPYBbUrJqZyprmYq/view">Google</a>
+                </p>
+            </div>
         </section>
     );
 }
