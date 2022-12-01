1aa54a0638c8ba7525bab7e10932bafc4d11a513
Tyler M <tmorse2222@gmail.com>
2022-11-30 18:36:30 -0700

    Added link to Resume and proficiencies in Resume component

--- portfolio/src/components/pages/Resume.js
+++ portfolio/src/components/pages/Resume.js
@@ -3,9 +3,13 @@ import React from "react";
 export default function Resume() {
   return (
     <section>
-      <h1 id="resume">Resume</h1>
+      <h1 id="resume">Developer Proficiencies and Resume</h1>
       <p>
-        This section will contain a downloadable copy of my resume.
+        My skills include: Python, JavaScript, HTML, CSS, React, Node.js, Express, MySQL, MongoDB, and more.
+      </p>
+      <p>
+        A full copy of my resume can be found
+        <a href="https://www.linkedin.com/in/tyler-morse-ab8069241/overlay/1635503727043/single-media-viewer/"> here.</a>
       </p>
     </section>
   );
